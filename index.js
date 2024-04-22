0d4c67b78d43b4603d482ba57b4254814291f5ee
ANZIE05 <161513339+ANZIE05@users.noreply.github.com>
2024-04-22 10:28:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+//  CALCULATOR(HOME WORK)
+// ADDITION
+// import inquirer from "inquirer";
+// const input1 = await inquirer.prompt({
+//    name: "abc",
+//    type: "number",
+//    message: "Enter your first number",
+// })
+// const input2 = await inquirer.prompt({
+//     name: "xyz",
+//     type: "number",
+//     message: "Enter your second number",
+//  })
+//  let calculate: number = input1.abc + input2.xyz
+//  console.log(calculate);
+// SUBTRACTION
+// import inquirer from "inquirer";
+// const input1 = await inquirer.prompt({
+//    name: "abc",
+//    type: "number",
+//    message: "Enter your first number",
+// })
+// const input2 = await inquirer.prompt({
+//     name: "xyz",
+//     type: "number",
+//     message: "Enter your second number",
+//  })
+//  let calculate: number = input1.abc - input2.xyz
+//  console.log(calculate);
+// MULTIPLICATION
+// import inquirer from "inquirer";
+// const input1 = await inquirer.prompt({
+//    name: "abc",
+//    type: "number",
+//    message: "Enter your first number",
+// })
+// const input2 = await inquirer.prompt({
+//     name: "xyz",
+//     type: "number",
+//     message: "Enter your second number",
+//  })
+//  let calculate: number = input1.abc * input2.xyz
+//  console.log(calculate);
+// DIVISION
+// import inquirer from "inquirer";
+// const input1 = await inquirer.prompt({
+//     name: "abc",
+//     type: "number",
+//     message: "Enter your first number",
+// })
+// const input2 = await inquirer.prompt({
+//     name: "xyz",
+//     type: "number",
+//     message: "Enter your second number",
+// })
+// let calculate: number = input1.abc / input2.xyz
+// console.log(calculate);
+// BMI CALCULATOR (HOME WORK)
+import chalk from "chalk";
+let weigthInKg = 65;
+let heightInMeters = 2;
+let bmiResult = weigthInKg / (heightInMeters * heightInMeters);
+console.log(chalk.bgCyan.bold.underline(bmiResult));
